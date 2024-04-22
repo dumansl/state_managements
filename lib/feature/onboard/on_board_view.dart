51f7076b932cc2f50389325367900bc7a200b102
Şule Duman <sduman_816@hotmail.com>
2024-04-22 18:18:23 +0300

    Initial commit

---
+++ lib/feature/onboard/on_board_view.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:state_managements/feature/onboard/widget/onboard_card.dart';
+import 'package:state_managements/product/page_padding.dart';
+
+import 'onboard_model.dart';
+import 'tab_indicator.dart';
+
+part './module/start_fab_button.dart';
+
+class OnBoardView extends StatefulWidget {
+  const OnBoardView({Key? key}) : super(key: key);
+
+  @override
+  _OnBoardViewState createState() => _OnBoardViewState();
+}
+
+class _OnBoardViewState extends State<OnBoardView> {
+  final String _skipTitle = 'Skip';
+
+  int _selectedIndex = 0;
+
+  bool get _isLastPage =>
+      OnBoardModels.onBoardItems.length - 1 == _selectedIndex;
+  bool get _isFirstPage => _selectedIndex == 0;
+
+  // ---xx
+  ValueNotifier<bool> isBackEnable = ValueNotifier(false);
+  // --xx
+
+  void _incrementAndChange([int? value]) {
+    if (_isLastPage && value == null) {
+      _changeBackEnable(true);
+      return;
+    }
+
+    _changeBackEnable(false);
+    _incrementSelectedPage(value);
+  }
+
+  void _changeBackEnable(bool value) {
+    if (value == isBackEnable.value) return;
+    isBackEnable.value = value;
+  }
+
+  void _incrementSelectedPage([int? value]) {
+    setState(() {
+      if (value != null) {
+        _selectedIndex = value;
+      } else {
+        _selectedIndex++;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar(),
+      body: Padding(
+        padding: const PagePadding.all(),
+        child: Column(
+          children: [
+            Expanded(child: _pageViewItems()),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TabIndicator(selectedIndex: _selectedIndex),
+                _StartFabButton(
+                  isLastPage: _isLastPage,
+                  onPressed: () {
+                    _incrementAndChange();
+                  },
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      systemOverlayStyle: SystemUiOverlayStyle.dark,
+      actions: [
+        ValueListenableBuilder<bool>(
+          valueListenable: isBackEnable,
+          builder: (BuildContext context, bool value, Widget? child) {
+            return value
+                ? const SizedBox()
+                : TextButton(
+                    onPressed: () {},
+                    child: Text(_skipTitle),
+                  );
+          },
+        ),
+      ],
+      leading: _isFirstPage
+          ? null
+          : IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.chevron_left_outlined,
+                color: Colors.grey,
+              ),
+            ),
+    );
+  }
+
+  Widget _pageViewItems() {
+    return PageView.builder(
+      onPageChanged: (value) {
+        _incrementAndChange(value);
+      },
+      itemCount: OnBoardModels.onBoardItems.length,
+      itemBuilder: (context, index) {
+        return OnBoardCard(model: OnBoardModels.onBoardItems[index]);
+      },
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:state_managements/feature/onboard/on_board_view.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: const OnBoardView(),
+      theme: ThemeData.light().copyWith(
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Color.fromRGBO(11, 23, 84, 1),
+          foregroundColor: Colors.white,
+        ),
+      ),
+    );
+  }
+}
